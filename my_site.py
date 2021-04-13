9dd794affc8ece018a4cc99b8fb8a052a4b2e522
esgalimov <esgalimov@gmail.com>
2021-04-13 22:10:13 +0300

    фронт страница аукциона

--- my_site.py
+++ my_site.py
@@ -38,5 +38,10 @@ def start():
     return render_template('main_page.html', **params)
 
 
+@app.route('/auction')
+def auction_page():
+    return  render_template('auction_page.html', **params)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
