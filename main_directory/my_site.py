a3d077f0871ecd69d08c760922c3e9ce47e2be4c
esgalimov <esgalimov@gmail.com>
2021-04-15 00:21:07 +0300

    Рега версия 1

--- main_directory/my_site.py
+++ main_directory/my_site.py
@@ -3,8 +3,8 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from sqalch_data.data.db_session import *
-from sqalch_data.data.__all_models import *
+from pure_api.sqalch_data.data.db_session import *
+from pure_api.sqalch_data.data.__all_models import *
 import json
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 import datetime
@@ -39,8 +39,37 @@ def start():
 
 @app.route('/auction')
 def auction_page():
-    return  render_template('auction_page.html', **params)
+    return render_template('auction_page.html', **params)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterUser()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('rega.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('rega.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            patronymic=form.patronymic.data,
+            age=form.age.data,
+            position=form.position.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('rega.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
+    global_init('../pure_api/sqalch_data/db/main_database.db')
     app.run(port=8080, host='127.0.0.1')
